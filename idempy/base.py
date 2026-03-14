b1ff6d9a05b72db9732db459c49b6d1078630530
Ashleigh Hewitt <tom_94_oasis@hotmail.com>
2026-03-14 11:04:14 +0000

    base added

---
+++ idempy/base.py
@@ -0,0 +1,26 @@
+from typing import Callable
+from abc import ABC, abstractmethod
+from idempy.models import IdempotencyKey, Status
+
+class Base(ABC): 
+    @abstractmethod
+    def get(self, key: str, IdempotencyKey: IdempotencyKey) -> None:
+        self.idempotency_key = IdempotencyKey
+        if not self.idempotency_key:
+            raise IdempotencyError("Idempotency key not found")
+
+    @abstractmethod
+    def create_in_progress(self, key: str, fingerprint: str) -> Bool:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def mark_completed(self, key: str, fingerprint: str, result_data: bytes, result_status: int) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def mark_failed(self, key: str, fingerprint: str, result_error: str) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def mark_pending(self, key: str, fingerprint: str) -> bool:
+        raise NotImplementedError
