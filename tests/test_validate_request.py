03268c04ba82d772e70fa5452cfa7c84d5def65a
Ashleigh Hewitt <tom_94_oasis@hotmail.com>
2026-03-18 20:56:34 +0000

    adding test to core

---
+++ tests/test_validate_request.py
@@ -0,0 +1,27 @@
+from idempy.core import Core
+from pytest import fixture
+
+@fixture
+def core():
+    return Core()
+def test_validate_request(core):
+    request = {
+        'idempotency_key': '1234567890',
+        'fingerprint': '1234567890',
+    }
+    assert core.validate_request(request) is True
+
+def test_validate_request_failure(core): 
+    request = {
+        'idempotency_key': None,
+        'fingerprint': '1234567890',
+    }
+    assert core.validate_request(request) is False
+
+def test_validate_fingerprint(core):
+    fingerprint = '1234567890'
+    assert core.validate_fingerprint(fingerprint) is True
+
+def test_validate_fingerprint_failure(core):
+    fingerprint = None
+    assert core.validate_fingerprint(fingerprint) is False
